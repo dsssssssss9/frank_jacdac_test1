a1b3c5015833ef500a4d76c8ee22e952a4619b3f
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2024-02-10 20:19:39 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+modules.button1.onEvent(jacdac.ButtonEvent.Down, function () {
+    basic.showIcon(IconNames.Yes)
+})
+modules.button1.onEvent(jacdac.ButtonEvent.Up, function () {
+    basic.showIcon(IconNames.StickFigure)
+})
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
